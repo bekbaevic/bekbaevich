05261ad886ac9c3ae7495a53a00cc15e416d5f86
bekbaevic <iseytmuratov77@gmail.com>
2024-05-30 00:34:34 +0500

    height

--- src/components/MyLife.jsx
+++ src/components/MyLife.jsx
@@ -7,7 +7,7 @@ const MyLife = () => {
   const { colorMode, toggleColorMode } = useColorMode()
 
   return (
-    <div id='about' className='pt-[70px]'>
+    <div id='about' className=''>
       <div className='h-full md:h-[calc(100vh-70px)] w-full relative md:rotate-0'>
         <img src={Banner} alt="my photo" className='max-h-full w-full object-cover object-bottom mx-auto' />
         <span className='absolute top-0 left-0 h-full w-full' style={colorMode === "light" ? {background: "#ffffff30"} : {backgroundColor: "#00000050"}} />
