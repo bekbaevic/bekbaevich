2d44ec682dd7b659063462a916330e9ad772e241
bekbaevic <iseytmuratov77@gmail.com>
2024-06-03 01:21:17 +0500

    project

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import MyLife from './components/MyLife'
 import Skills from './components/Skills'
 import { useLocation } from 'react-router-dom'
 import { useLayoutEffect } from 'react'
-import ProjectsComponent from './components/Projects'
+import ProjectsComponent from './components/ProjectsComponent'
 
 function App() {
   const { isOpen, onOpen, onClose } = useDisclosure()
