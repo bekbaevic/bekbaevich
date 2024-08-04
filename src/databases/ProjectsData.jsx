52205a0d7b50d6e6fecf1d3288ba64cfb03e96b5
bekbaevic <iseytmuratov77@gmail.com>
2024-08-04 23:25:08 +0500

    delete unnecessary projects

--- src/databases/ProjectsData.jsx
+++ src/databases/ProjectsData.jsx
@@ -12,14 +12,7 @@ import tetris from "../images/project_images/tetris.png";
 import weather from "../images/project_images/weather.png";
 import kiyimKenshek from "../images/project_images/kiyim-kenshek.png"
 export const ProjectsData = [
-  {
-    id: 122,
-    image: kiyimKenshek,
-    url: "https://kiyim-kenshek.vercel.app/",
-    title: "Kiyim-kenshek onlayn magazin",
-    category: "shop",
-    delay: 0.3
-  },
+
   {
     id: 121, 
     image: usersDashboard,
@@ -37,72 +30,10 @@ export const ProjectsData = [
     delay: 0.6
   },
   {
-    id: 5,
-    image: onlineShop,
-    url: "https://onlineshop-dusky.vercel.app/",
-    title: "Online shop",
-    category: "shop",
-  },
-  {
-    id: 6,
-    image: onlineShopDasboard,
-    url: "https://onlineshop-dashboard-six.vercel.app/",
-    title: "Online shop dashboard",
-    category: "shop"
-  },
-  {
-    id: 10,
-    image: tetris,
-    url: "https://vocal-naiad-a76436.netlify.app/",
-    title: "Tetris",
-    category: "games"
-  },
-  {
-    id: 8,
-    image: quizTime,
-    url: "https://quiztime-5crf.vercel.app/",
-    title: "Quiz time",
-    category: "games"
-  },
-  {
-    id: 11,
-    image: weather,
-    url: "https://dashing-vacherin-90025b.netlify.app/",
-    title: "Weather app",
-    category: "dashboard"
-  },
-  // {
-  //   id: 1,
-  //   image: events,
-  //   url: "https://eventswithbekbaevich.netlify.app/",
-  //   title: "Events",
-  // },
-  {
     id: 2,
     image: form,
     url: "https://sparkly-basbousa-e01fbb.netlify.app/",
     title: "Form Component",
     category: "components"
-  },
-  {
-    id: 3,
-    image: jsProject,
-    url: "https://effervescent-bubblegum-63b75e.netlify.app/",
-    title: "JS project",
-    category: "dashboard"
-  },
-  {
-    id: 4,
-    image: onlineBatteryDetector,
-    url: "https://devicebatterydetector.netlify.app/",
-    title: "Online battery detector",
-    category: "dashboard"
-  },
-  {
-    id: 7,
-    image: pixelArt,
-    url: "https://pixelartgenerate.netlify.app/",
-    title: "Pixel art",
-    category: "games"
   },
 ];
